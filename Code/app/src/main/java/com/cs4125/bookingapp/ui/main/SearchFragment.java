239cda9bb1b48ebbcb51831c882f0ab195a3fda5
Damian Skrzypek <damianskrzypek351@interia.eu>
2020-11-13 11:11:23 +0000

    Fix SearchFragment

--- Code/app/src/main/java/com/cs4125/bookingapp/ui/main/SearchFragment.java
+++ Code/app/src/main/java/com/cs4125/bookingapp/ui/main/SearchFragment.java
@@ -1,7 +1,11 @@
 package com.cs4125.bookingapp.ui.main;
 
+import androidx.lifecycle.ViewModelProvider;
+
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -10,62 +14,28 @@ import android.view.ViewGroup;
 
 import com.cs4125.bookingapp.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link SearchFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class SearchFragment extends Fragment
 {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private SearchViewModel mViewModel;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public SearchFragment()
+    public static SearchFragment newInstance()
     {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment SearchFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static SearchFragment newInstance(String param1, String param2)
-    {
-        SearchFragment fragment = new SearchFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
+        return new SearchFragment();
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState)
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState)
     {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null)
-        {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+        return inflater.inflate(R.layout.search_fragment, container, false);
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+    public void onActivityCreated(@Nullable Bundle savedInstanceState)
     {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_search, container, false);
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = new ViewModelProvider(this).get(SearchViewModel.class);
+        // TODO: Use the ViewModel
     }
+
 }
---
+++ Code/app/src/main/java/com/cs4125/bookingapp/ui/main/SearchViewModel.java
@@ -0,0 +1,8 @@
+package com.cs4125.bookingapp.ui.main;
+
+import androidx.lifecycle.ViewModel;
+
+public class SearchViewModel extends ViewModel
+{
+    // TODO: Implement the ViewModel
+}
