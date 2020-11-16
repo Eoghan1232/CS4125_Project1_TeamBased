bf6ef72cd4df62165c2af11ca5d839b2f3c09766
Eoghan Russell <eoghanrussell1212@gmail.com>
2020-11-16 20:16:34 +0000

    Started AWS Implementation
    Setup Amplify
    Model for Booking as an example.
    Need to complete other models.
    Setup Interfaces and classes.
    Started working through getting AWS setup, DataStore next and then will connect it to the cloud.
    Setup AWS account also.

--- Code/app/src/main/java/com/cs4125/bookingapp/repositories/IPaymentRepository.java
+++ Code/app/src/main/java/com/cs4125/bookingapp/repositories/IPaymentRepository.java
@@ -1,5 +1,10 @@
 package com.cs4125.bookingapp.repositories;
 
+import com.cs4125.bookingapp.entities.TransactionRecord;
+
 public interface IPaymentRepository
 {
+    public boolean getTransaction(TransactionRecord transaction);
+    public boolean insertTransaction(TransactionRecord transaction);
+    public boolean updateTransaction(TransactionRecord transaction);
 }
--- Code/app/src/main/java/com/cs4125/bookingapp/repositories/IUserRepository.java
+++ Code/app/src/main/java/com/cs4125/bookingapp/repositories/IUserRepository.java
@@ -1,5 +1,10 @@
 package com.cs4125.bookingapp.repositories;
 
+import com.cs4125.bookingapp.entities.User;
+
 public interface IUserRepository
 {
+    public boolean insertUser(User user);
+    public boolean getUser(User user);
+    public boolean updateUser(User user);
 }
