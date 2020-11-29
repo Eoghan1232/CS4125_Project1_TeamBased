327d99bde10eff34d2d00d93e80a76168c718a18
Eoghan1232 <eoghanrussell1212@gmail.com>
2020-11-29 18:06:39 +0000

    Setting Up JUnit Tests

---
+++ Spring/bookingapp/src/test/java/com/cs4125/bookingapp/controller/UserTest.java
@@ -0,0 +1,18 @@
+package com.cs4125.bookingapp.controller;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class UserTest {
+
+
+
+}
