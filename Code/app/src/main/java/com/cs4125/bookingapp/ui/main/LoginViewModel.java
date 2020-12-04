f62b5100d5a53693f585ef1da303cb411dd32f53
Damian Skrzypek <damianskrzypek351@interia.eu>
2020-12-04 18:23:06 +0000

    Connected Android app with Springboot
    Finished implementation of retrofit singleton class and the webservice interface.

--- Code/app/src/main/java/com/cs4125/bookingapp/ui/main/LoginViewModel.java
+++ Code/app/src/main/java/com/cs4125/bookingapp/ui/main/LoginViewModel.java
@@ -2,17 +2,19 @@ package com.cs4125.bookingapp.ui.main;
 
 import androidx.lifecycle.ViewModel;
 
+import com.cs4125.bookingapp.repositories.UserRepository;
+
 public class LoginViewModel extends ViewModel
 {
     // TODO: Implement the ViewModel
-    private UserDoa repository;
+    private UserRepository repository;
 
-    public void init(UserDoa repository){
+    public void init(UserRepository repository){
         this.repository = repository;
     }
 
-    public LiveData<ResponseBody> login(UserCredential credential){
-        return repository.loginUser(credential);
-    }
+//    public LiveData<ResponseBody> login(UserCredential credential){
+//        return repository.loginUser(credential);
+//    }
 
 }
