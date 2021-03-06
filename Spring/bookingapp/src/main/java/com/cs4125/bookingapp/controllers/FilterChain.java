dcfed2d8a87942a8a0ee6aeed34bffb422d440a1
Pawros21 <17214211@studentmail.ul.ie>
2021-03-06 15:16:51 +0000

    Added FilterChain and FilterManager
    Committing Filterchain class and FilterManager class for furhter work with Eoghan on Interceptor

---
+++ Spring/bookingapp/src/main/java/com/cs4125/bookingapp/controllers/FilterChain.java
@@ -0,0 +1,22 @@
+package com.cs4125.bookingapp.controllers;
+
+public class FilterChain {
+    private List<Filter> filters = new ArrayList<Filter>();
+    private Target target;
+
+    public void addFilter(Filter filter){
+        filters.add(filter);
+    }
+
+    public String execute(String request){
+        for (Filter filter : filters) {
+            filter.execute(request);
+        }
+        return target.execute(request);
+        // We return the String as that how our system runs but for the future the filters will be able to alter the result string as well
+    }
+
+    public void setTarget(Target target){
+        this.target = target;
+    }
+}
---
+++ Spring/bookingapp/src/main/java/com/cs4125/bookingapp/controllers/FilterManager.java
@@ -0,0 +1,19 @@
+package com.cs4125.bookingapp.controllers;
+
+public class FilterManager {
+    FilterChain filterChain;
+
+    public FilterManager(Object target){
+        filterChain = new FilterChain();
+        filterChain.setTarget(target);
+    }
+    public void setFilter(Filter filter){
+        filterChain.addFilter(filter);
+    }
+
+    public String filterRequest(String request){
+        String result = filterChain.execute(request);
+        // We return the String as that how our system runs but for the future the filters will be able to alter the result string as well
+        return result;
+    }
+}
