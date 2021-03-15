029bfe4916c66ff9da93291757fce3e3f6815519
Eoghan1232 <eoghanrussell1212@gmail.com>
2021-03-15 21:42:10 +0000

    Updating JUnit Tests
    Fixed JUnit tests to work.
    Added MVN Dependencies.
    MVN will now run the tests when it builds the project.

--- Spring/bookingapp/src/test/java/com/cs4125/bookingapp/controller/UserTest.java
+++ Spring/bookingapp/src/test/java/com/cs4125/bookingapp/controller/UserTest.java
@@ -1,67 +1,74 @@
 package com.cs4125.bookingapp.controller;
 
-import static org.junit.Assert.assertEquals;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
+
 import com.cs4125.bookingapp.controllers.UserController;
+import com.cs4125.bookingapp.model.ConcreteUserFactory;
 import com.cs4125.bookingapp.model.UserFactory;
 import com.cs4125.bookingapp.model.entities.User;
+import com.cs4125.bookingapp.model.repositories.LogRepository;
+import com.cs4125.bookingapp.model.repositories.UserRepository;
 import com.cs4125.bookingapp.services.UserServiceImpl;
-import com.cs4125.bookingapp.services.interceptor.FilterChain;
 import com.cs4125.bookingapp.services.interceptor.FilterManager;
 import com.cs4125.bookingapp.services.interceptor.LogFilter;
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.MockitoJUnitRunner;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
-@RunWith(MockitoJUnitRunner.Silent.class)
+//This creates a DB in memory so it doesn't affect the real DB.
+@DataJpaTest
 public class UserTest {
 
-    @Mock
     private FilterManager myManager;
-    @Mock
-    private FilterChain filterChain;
-    @Mock
     private LogFilter logFilter;
-    @Mock
     private UserServiceImpl userServiceMock;
-    @Mock
+    private UserController userControllerMock;
     private UserFactory userFactory;
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private LogRepository logRepository;
 
-    UserController userControllerMock;
-
-    @Before
+    @BeforeEach
     public void init(){
-        userControllerMock = new UserController(userServiceMock, myManager, logFilter);
+        myManager = new FilterManager();
+        logFilter = new LogFilter(logRepository);
+        userFactory = new ConcreteUserFactory();
+        userServiceMock = new UserServiceImpl(userRepository, new ConcreteUserFactory());
+        userControllerMock = new UserController(userServiceMock,myManager,logFilter);
+    }
+
+    @Test
+    public void temptTesting(){
+        User mockUser = new User("Mock","password","mock@gmail.com",1);
+        String message = userServiceMock.register(mockUser);
+        assertEquals("SUCCESS", message.split(":")[0]);
     }
 
     @Test
     public void registerUserTest() {
-    User mockUser = new User("Mock","password","mock@gmail.com",1);
-    when(userFactory.getUser("NORMAL_USER","Mock","password","mock@gmail.com")).thenReturn(mockUser);
-    when(userServiceMock.register(mockUser)).thenReturn("Success");
     String message = userControllerMock.addNewUser("Mock","password","mock@gmail.com");
-    assertEquals("Success",message);
-    }
-
-    @Test
-    public void LoginTest() {
-        String mockUsername = "Dummy";
-        String mockPassword = "testing";
-        when(userServiceMock.login(mockUsername,mockPassword)).thenReturn("Success");
-        String message = userControllerMock.getUser(mockUsername,mockPassword);
-        assertEquals("Success", message);
+    assertEquals("SUCCESS",message.split(":")[0]);
     }
 
     @Test
     public void loginFailTest() {
-        String mockUsername = "test";
-        String mockPassword = "test";
-        when(userServiceMock.login(mockUsername,mockPassword)).thenReturn("Failure");
+        String mockUsername = "MOCKUSERNAMETHATNOONEWILLEVERGET";
+        String mockPassword = "RANDOMPASSWORDTHATNOONEWILLEVERGET";
         String message = userControllerMock.getUser(mockUsername,mockPassword);
-        assertEquals("Failure", message);
+        assertEquals("FAILURE", message.split(":")[0]);
+    }
+
+    @Test
+    public void LoginTest() { ;
+        userControllerMock.addNewUser("dummy","testing","dummytesting@gmail.com");
+        String mockUsername = "dummy";
+        String mockPassword = "testing";
+        String message = userControllerMock.getUser(mockUsername,mockPassword);
+        assertEquals("SUCCESS", message.split(":")[0]);
     }
 
 }
